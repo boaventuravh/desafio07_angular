5a5617c1e86b518bc5057dd62e1a03e06d4e1546
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-05-13 11:08:59 -0300

    corrigir rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginFormComponent } from "./components/login-form/login-form.component";
-import { LoginPageComponent } from "./components/login-page/login-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LoginFormComponent, RouterOutlet, LoginPageComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  template: `
-    <router-outlet />
-  `
+  styleUrl: './app.component.css',  
 })
-export class AppComponent {
-  title = 'desafio07_angular';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo:'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./components/login-page/login-page.component')
+            .then(c => c.LoginPageComponent);
+        },
+    },
+    {
+        path: 'home',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./components/home-page/home-page.component')
+            .then(c => c.HomePageComponent);
+        },
+    },
+];
