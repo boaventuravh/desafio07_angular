bdc53b518ac7c68a0a0b358f4d61104935a854ac
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-05-13 08:54:43 -0300

    retirar pag login do app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginFormComponent } from "./components/login-form/login-form.component";
+import { LoginPageComponent } from "./components/login-page/login-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent, RouterOutlet, LoginPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  template: `
+    <router-outlet />
+  `
 })
 export class AppComponent {
   title = 'desafio07_angular';
