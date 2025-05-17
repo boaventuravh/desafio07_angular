ba2affd561de24e8b57b26e83ece3631da90d78a
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-05-17 01:03:18 -0300

    separar welcome da home em outro componente

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HomeWelcomeComponent } from "../home-welcome/home-welcome.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [HomeWelcomeComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
