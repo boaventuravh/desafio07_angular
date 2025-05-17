c51656f810a60443558339078b4f66ebd3f75ad2
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-05-16 22:27:48 -0300

    corrigir rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { DashboardPageComponent } from './components/dashboard-page/dashboard-page.component';
 
 export const routes: Routes = [
+    
     {
         path: '',
-        redirectTo:'login',
-        pathMatch: 'full'
-    },
-    {
-        path: 'login',
-        pathMatch: 'full',
-        loadComponent: () => {
-            return import('./components/login-page/login-page.component')
-            .then(c => c.LoginPageComponent);
-        },
+        pathMatch: 'full',        
+        component: LoginPageComponent
     },
     {
         path: 'home',
         pathMatch: 'full',
-        loadComponent: () => {
-            return import('./components/home-page/home-page.component')
-            .then(c => c.HomePageComponent);
-        },
+        component: HomePageComponent        
     },
+    {
+        path: 'dashboard',
+        pathMatch: 'full',
+        component: DashboardPageComponent
+    }
 ];
