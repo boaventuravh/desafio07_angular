02083dbbde55b6bc6102dd066e7fd840cddd7a17
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-05-16 23:56:01 -0300

    implementar login

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Usuario } from '../models/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ import { Usuario } from '../models/user';
 export class LoginService {
   http = inject(HttpClient)
 
-  login(nome: string, senha: string) { 
+  login(nome: string, senha: string): Observable<Usuario> { 
     return this.http.post<Usuario>(
       "http://localhost:3001/login",
       { nome, senha }
