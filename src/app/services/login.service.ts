d3ab7f85a0c8242d0bbcd958feeca254be23a7c3
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-05-16 22:50:22 -0300

    criar método login

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Usuario } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  http = inject(HttpClient)
 
-  constructor() { }
+  login(nome: string, senha: string) { 
+    return this.http.post<Usuario>(
+      "http://localhost:3001/login",
+      { nome, senha }
+    )
+  }
 }
