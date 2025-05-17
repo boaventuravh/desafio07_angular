56d87e930121d47edda7496004f151c0808bf5f4
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-05-17 02:31:53 -0300

    criar sidebar

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeWelcomeComponent } from "../home-welcome/home-welcome.component";
+import { HomeSidebarComponent } from "../home-sidebar/home-sidebar.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [HomeWelcomeComponent],
+  imports: [HomeWelcomeComponent, HomeSidebarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
