4eb7f2ec22b6738aa65c2ec19dc4b60072102acb
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-05-18 06:44:12 -0300

    implementar toogleSidebar

--- src/app/components/home-sidebar/home-sidebar.component.ts
+++ src/app/components/home-sidebar/home-sidebar.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home-sidebar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-sidebar.component.html',
   styleUrl: './home-sidebar.component.css'
 })
-export class HomeSidebarComponent {
 
+export class HomeSidebarComponent {
+    open: boolean;
+    sidebarPosition: string;
+
+    constructor(){
+      this.open = false;
+      this.sidebarPosition = '-translate-x-full';
+    }
+
+    toggleSidebar(state: boolean) {
+    this.open = state;
+    this.sidebarPosition = (state ? 'translate-x-0' : '-translate-x-full')
+    }
 }
