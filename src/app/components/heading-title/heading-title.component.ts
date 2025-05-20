4b7bfd59a6c1e9562946c62205d97e6840e89383
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-05-20 18:47:57 -0300

    criar heading title

---
+++ src/app/components/heading-title/heading-title.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heading-title',
+  imports: [],
+  templateUrl: './heading-title.component.html',
+  styleUrl: './heading-title.component.css'
+})
+export class HeadingTitleComponent {
+
+}
