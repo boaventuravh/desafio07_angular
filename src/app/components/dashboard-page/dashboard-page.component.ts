5d523d2a63090e5fbee319d3f496bd4d9b132eaa
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-05-20 19:57:35 -0300

    adicionar imports necessários

--- src/app/components/dashboard-page/dashboard-page.component.ts
+++ src/app/components/dashboard-page/dashboard-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { DashboardCardComponent } from "../dashboard-card/dashboard-card.component";
+import { HomeSidebarComponent } from "../home-sidebar/home-sidebar.component";
+import { HeadingTitleComponent } from "../heading-title/heading-title.component";
+import { DashboardTableComponent } from "../dashboard-table/dashboard-table.component";
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [DashboardCardComponent, HomeSidebarComponent, HeadingTitleComponent, DashboardTableComponent],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css'
 })
