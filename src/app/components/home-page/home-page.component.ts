e667df7c2f83d9949dd5093e60aaaa6ca1dad464
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-05-22 03:26:00 -0300

    importar heading-title

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeWelcomeComponent } from "../home-welcome/home-welcome.component";
 import { HomeSidebarComponent } from "../home-sidebar/home-sidebar.component";
+import { HeadingTitleComponent } from "../heading-title/heading-title.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [HomeWelcomeComponent, HomeSidebarComponent],
+  imports: [HomeWelcomeComponent, HomeSidebarComponent, HeadingTitleComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
