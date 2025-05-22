c7e72ea16316432a5d202450bfbde75684f07411
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2025-05-22 03:30:28 -0300

    criar complaints-page

---
+++ src/app/components/complaints-page/complaints-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-complaints-page',
+  imports: [],
+  templateUrl: './complaints-page.component.html',
+  styleUrl: './complaints-page.component.css'
+})
+export class ComplaintsPageComponent {
+
+}
